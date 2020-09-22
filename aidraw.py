dd4b4c1b3c1c50786612230810ded48321bee05c
Emma Algotsson <Emma@iMac.lan>
2020-09-22 13:20:20 +0200

    added classes

--- aidraw.py
+++ aidraw.py
@@ -1,4 +1,13 @@
 # import packets
-import numpy as np
+import numpy as np #to process image matrices
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow import keras
+import matplotlib.pyplot as plt #display result
 
-# load dataset 
+# load dataset
+f = open("miniCategories.txt", "r") #52 classes with animals/living things
+# For reading use
+classes = f.readinglines()
+f.close()
+classes = [c.replace('\n', '').replace(' ', '_') for c in classes]
